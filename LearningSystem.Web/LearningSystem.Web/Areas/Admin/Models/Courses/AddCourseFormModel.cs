8b0a959aec6bf91b0a2f3af6727c5c388db09e95
HristoforEnchev <35043525+HristoforEnchev@users.noreply.github.com>
2018-02-07 23:10:11 +0200

    Added TempData Extension and Course Controller

--- LearningSystem.Web/LearningSystem.Web/Areas/Admin/Models/Courses/AddCourseFormModel.cs
+++ LearningSystem.Web/LearningSystem.Web/Areas/Admin/Models/Courses/AddCourseFormModel.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace LearningSystem.Web.Areas.Admin.Models.Courses
+﻿namespace LearningSystem.Web.Areas.Admin.Models.Courses
 {
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    using static Data.DataConstants;
+
     public class AddCourseFormModel
     {
+        [Required]
+        [MinLength(CourseNameMaxLength)]
+        public string Name { get; set; }
 
+        [Required]
+        [MaxLength(CourseDescriptionMaxLength)]
+        public string Description { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public string TrainerId { get; set; }
     }
 }
